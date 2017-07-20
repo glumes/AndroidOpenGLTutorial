19425f90ab4f5e584c0872cd54b05fa1348ba252
glumes <zhaoying9402@gmail.com>
2017-07-20 23:47:12 +0800

    init project

--- app/src/main/java/com/glumes/androidopengltutorial/MainActivity.java
+++ app/src/main/java/com/glumes/androidopengltutorial/MainActivity.java
@@ -1,29 +1,28 @@
 package com.glumes.androidopengltutorial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+
+    @BindView(R.id.sample_text)
+    TextView sampleText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        sampleText.setText("Welcome to Android OpenGL ES !");
+        Timber.d("start Timber log cat");
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+
 }
