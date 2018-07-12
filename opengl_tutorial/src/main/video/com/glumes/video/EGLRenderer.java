b0c49d31d3c4d6c34730e74c41d70572cfd065cf
glumes <zhaoying9402@gmail.com>
2018-07-12 11:55:40 +0800

    replace video content by opengl

---
+++ opengl_tutorial/src/main/video/com/glumes/video/EGLRenderer.java
@@ -0,0 +1,123 @@
+package com.glumes.video;
+
+import android.opengl.EGL14;
+
+import javax.microedition.khronos.egl.EGL10;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLContext;
+import javax.microedition.khronos.egl.EGLDisplay;
+import javax.microedition.khronos.egl.EGLSurface;
+
+
+public class EGLRenderer {
+
+    private static final int CFG_RED_SIZE = 8;
+    private static final int CFG_GREEN_SIZE = 8;
+    private static final int CFG_BLUE_SIZE = 8;
+    private static final int CFG_ALPHA_SIZE = 8;
+    private static final int CFG_DEPTH_SIZE = 16;
+    private static final int CFG_STENCIL_SIZE = 0;
+
+    private static final int[] EGL_CONFIGS = {
+            EGL10.EGL_RED_SIZE, CFG_RED_SIZE,
+            EGL10.EGL_GREEN_SIZE, CFG_GREEN_SIZE,
+            EGL10.EGL_BLUE_SIZE, CFG_BLUE_SIZE,
+            EGL10.EGL_ALPHA_SIZE, CFG_ALPHA_SIZE,
+            EGL10.EGL_DEPTH_SIZE, CFG_DEPTH_SIZE,
+            EGL10.EGL_STENCIL_SIZE, CFG_STENCIL_SIZE,
+            EGL10.EGL_RENDERABLE_TYPE, EGL14.EGL_OPENGL_ES2_BIT,
+            EGL10.EGL_NONE,
+    };
+
+    private static final int[] EGL_ATTRIBUTE = {
+            EGL14.EGL_CONTEXT_CLIENT_VERSION, 2,
+            EGL10.EGL_NONE,
+    };
+
+    private EGL10 mEGL = null;
+
+    private EGLContext mEGLContext = EGL10.EGL_NO_CONTEXT;
+
+    private EGLDisplay mEGLDisplay = EGL10.EGL_NO_DISPLAY;
+
+    private EGLSurface mEGLSurface = EGL10.EGL_NO_SURFACE;
+
+    public void create(int w, int h) {
+        int[] surfaceAttr = {
+                EGL10.EGL_WIDTH, w,
+                EGL10.EGL_HEIGHT, h,
+                EGL10.EGL_NONE,
+        };
+
+        mEGL = (EGL10) EGLContext.getEGL();
+        mEGLDisplay = mEGL.eglGetDisplay(EGL10.EGL_DEFAULT_DISPLAY);
+        mEGL.eglInitialize(mEGLDisplay, null);
+
+        EGLConfig config = getEGLConfig(mEGL, mEGLDisplay);
+        mEGLContext = mEGL.eglCreateContext(mEGLDisplay, config, EGL10.EGL_NO_CONTEXT, EGL_ATTRIBUTE);
+        mEGLSurface = mEGL.eglCreatePbufferSurface(mEGLDisplay, config, surfaceAttr);
+        mEGL.eglMakeCurrent(mEGLDisplay, mEGLSurface, mEGLSurface, mEGLContext);
+    }
+
+    public void destroy() {
+        mEGL.eglMakeCurrent(mEGLDisplay, EGL10.EGL_NO_SURFACE, EGL10.EGL_NO_SURFACE, EGL10.EGL_NO_CONTEXT);
+        mEGL.eglDestroyContext(mEGLDisplay, mEGLContext);
+        mEGL.eglDestroySurface(mEGLDisplay, mEGLSurface);
+        mEGL.eglTerminate(mEGLDisplay);
+        mEGL = null;
+        mEGLContext = EGL10.EGL_NO_CONTEXT;
+        mEGLDisplay = EGL10.EGL_NO_DISPLAY;
+        mEGLSurface = EGL10.EGL_NO_SURFACE;
+    }
+
+    private static EGLConfig getEGLConfig(EGL10 egl, EGLDisplay display) {
+        int[] configCount = {0};
+        if (!egl.eglChooseConfig(display, EGL_CONFIGS, null, 0, configCount)) {
+            return null;
+        }
+
+        EGLConfig[] configs = new EGLConfig[configCount[0]];
+        if (!egl.eglChooseConfig(display, EGL_CONFIGS, configs, configCount[0], configCount)) {
+            return null;
+        }
+
+        return chooseEGLConfig(egl, display, configs, configCount[0]);
+    }
+
+    private static EGLConfig chooseEGLConfig(EGL10 egl, EGLDisplay display, EGLConfig[] configs, int count) {
+        if (configs == null) {
+            return null;
+        }
+
+        int[] value = new int[1];
+        for (int i = 0; i < count; ++i) {
+            EGLConfig config = configs[i];
+            if (config == null) {
+                continue;
+            }
+
+            int d = getConfigAttribute(egl, display, config, value, EGL10.EGL_DEPTH_SIZE, 0);
+            int s = getConfigAttribute(egl, display, config, value, EGL10.EGL_STENCIL_SIZE, 0);
+            if (d >= CFG_DEPTH_SIZE && s >= CFG_STENCIL_SIZE) {
+                int r = getConfigAttribute(egl, display, config, value, EGL10.EGL_RED_SIZE, 0);
+                int g = getConfigAttribute(egl, display, config, value, EGL10.EGL_GREEN_SIZE, 0);
+                int b = getConfigAttribute(egl, display, config, value, EGL10.EGL_BLUE_SIZE, 0);
+                int a = getConfigAttribute(egl, display, config, value, EGL10.EGL_ALPHA_SIZE, 0);
+                if (r == CFG_RED_SIZE && g == CFG_GREEN_SIZE && b == CFG_BLUE_SIZE && a == CFG_ALPHA_SIZE) {
+                    return config;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private static int getConfigAttribute(EGL10 egl, EGLDisplay display,
+                                          EGLConfig config, int[] value,
+                                          int attribute, int defaultValue) {
+        if (egl.eglGetConfigAttrib(display, config, attribute, value)) {
+            return value[0];
+        }
+        return defaultValue;
+    }
+}
--- opengl_tutorial/src/main/video/com/glumes/video/FrameReplaceActivity.kt
+++ opengl_tutorial/src/main/video/com/glumes/video/FrameReplaceActivity.kt
@@ -1,18 +1,46 @@
 package com.glumes.video
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
+import android.graphics.Bitmap
+import android.opengl.GLES20.*
+import android.opengl.Matrix
 import android.os.Bundle
-import android.view.View
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
+import com.glumes.importobject.AlphaTextureRect
+import com.glumes.importobject.TextureRect
 import com.glumes.openglbasicshape.R
+import com.glumes.openglbasicshape.utils.MatrixState
+import com.glumes.openglbasicshape.utils.TextureHelper
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.nio.IntBuffer
+import javax.microedition.khronos.opengles.GL10.GL_RGBA
+import javax.microedition.khronos.opengles.GL10.GL_UNSIGNED_BYTE
+
+
+fun Context.showToast(msg: String) {
+    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+}
 
 class FrameReplaceActivity : AppCompatActivity() {
 
 
+    private val width = 229
+    private val height = 314
+
     private val mResultImage by lazy {
         findViewById<ImageView>(R.id.resultImage)
     }
+
+    private var mOriginImage: TextureRect? = null
+    private var mReplaceImage: TextureRect? = null
+    private var mAlphaTextureRect: AlphaTextureRect? = null
+    private var mOriginTextureId: Int = 0
+    private var mReplaceTextureId: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,10 +54,105 @@ class FrameReplaceActivity : AppCompatActivity() {
 
     private fun glslChange() {
 
+        mResultImage.setImageResource(R.drawable.waiting)
+        replace(false)
     }
 
-    private fun blendChange() {
 
+    private fun blendChange() {
+        mResultImage.setImageResource(R.drawable.waiting)
+
+        replace(true)
+    }
+
+
+    private fun replace(isBlend: Boolean) {
+        Observable.fromCallable {
+            return@fromCallable initEgl()
+        }.map {
+            prepare(width, height)
+            return@map it
+        }.map {
+            replaceContent(isBlend)
+            return@map it
+        }.map {
+            val result = readPixel(width, height)
+            it.destroy()
+            return@map result
+        }.subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    mResultImage.setImageBitmap(it)
+                }, {
+                    showToast("replace failed")
+                })
+    }
+
+    private fun prepare(w: Int, h: Int) {
+
+        mOriginImage = TextureRect(resources, 2.0f, 2.0f)
+        mReplaceImage = TextureRect(resources, 2.0f, 2.0f)
+        mAlphaTextureRect = AlphaTextureRect(resources, 2.0f, 2.0f)
+
+        mOriginTextureId = TextureHelper.loadTexture(this, R.drawable.origin)
+        mReplaceTextureId = TextureHelper.loadTexture(this, R.drawable.replace)
+
+        glViewport(0, 0, w, h)
+
+        MatrixState.setInitStack()
+
+        Matrix.setIdentityM(MatrixState.getVMatrix(), 0)
+
+        Matrix.setIdentityM(MatrixState.getProMatrix(), 0)
+
+    }
+
+    private fun initEgl(): EGLRenderer {
+        val renderer = EGLRenderer()
+        renderer.create(width, height)
+        return renderer
+    }
+
+    private fun replaceContent(isBlend: Boolean) {
+        glClearColor(1f, 1f, 1f, 1f)
+        glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
+        mOriginImage?.drawSelf(mOriginTextureId)
+
+        if (isBlend) {
+            glEnable(GL_BLEND);
+            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+            mReplaceImage?.drawSelf(mReplaceTextureId)
+            glDisable(GL_BLEND)
+        } else {
+            mAlphaTextureRect?.drawSelf(mReplaceTextureId)
+
+        }
+    }
+
+    private fun readPixel(width: Int, height: Int): Bitmap {
+
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+
+        val pixels = IntBuffer.allocate(width * height)
+
+        pixels.clear()
+
+        glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, pixels)
+
+        val pixelMirroredArray = IntArray(width * height)
+
+        pixels.position(0)
+
+        val pixelArray = pixels.array()
+
+        // read 之后的 pixel 上下是反过来的,
+        for (i in 0 until height) {
+            for (j in 0 until width) {
+                pixelMirroredArray[(height - i - 1) * width + j] = pixelArray[i * width + j]
+            }
+        }
+        bitmap.copyPixelsFromBuffer(IntBuffer.wrap(pixelMirroredArray))
+        return bitmap
     }
 
 }
